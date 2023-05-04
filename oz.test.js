0bb46784e8aba390cfa517964ecb76037f0c82f6
Andrew <andrew@nordicgamelab.org>
2023-05-04 13:35:54 -0400

    Update oz.test.js

--- oz.test.js
+++ oz.test.js
@@ -2,14 +2,14 @@ const convert = require('./convertlbs');
 const convertoz = convert.convertoz;
 
 
-test('Converts 5 ounces to kg and expects 0.141748 kg', () => {
-  expect(convertoz(5).kg).toBe(0.141748);
+test('Converts 5 ounces to kg and expects 0.1417474627204173 kg', () => {
+  expect(convertoz(5).kg).toBe(0.1417474627204173);
 });
 
 test('Converts 5 oz to lbs and expects 0.3125 lbs', () => {
   expect(convertoz(5).lbs).toBe(0.3125);
 });
 
-test('Converts 5 oz to grams and expects 141.748 grams', () => {
-  expect(convertoz(5).g).toBe(141.748);
+test('Converts 5 oz to grams and expects 141.75 grams', () => {
+  expect(convertoz(5).g).toBe(141.75);
 });
