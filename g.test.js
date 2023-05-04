a351ee1f0b1ec18ee85a0f0a0cb82109e4c7a2af
Andrew <andrew@nordicgamelab.org>
2023-05-04 13:44:14 -0400

    Update g.test.js

--- g.test.js
+++ g.test.js
@@ -6,10 +6,10 @@ test('Converts 5 grams to kg and expects 0.005 kg', () => {
   expect(convertg(5).kg).toBe(0.005);
 });
 
-test('Converts 5 grams to lbs and expects 0.0110231 lbs', () => {
-  expect(convertg(5).lbs).toBe(0.0110231);
+test('Converts 5 grams to lbs and expects 0.011022927689594356 lbs', () => {
+  expect(convertg(5).lbs).toBe(0.011022927689594356);
 });
 
-test('Converts 5 grams to grams and expects 0.17637 oz', () => {
-  expect(convertg(5).oz).toBe(0.17637);
+test('Converts 5 grams to grams and expects 0.1763668430335097 oz', () => {
+  expect(convertg(5).oz).toBe(0.1763668430335097);
 });
